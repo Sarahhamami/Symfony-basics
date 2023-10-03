280dfb566f51850e70fdfb4096217b1c81486702
Sarahhamami15@gmail.com <sarahhammami15@gmail.com>
2023-10-03 14:30:40 +0100

    update/ controller public variable array

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,6 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    public $authors = array(
+        array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
+        'victor.hugo@gmail.com ', 'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+        ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+        'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -26,32 +34,19 @@ class AuthorController extends AbstractController
     public function list(): Response
     {
 
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),
-            );
-          $nb= count($authors);
-          $sum = array_sum(array_column($authors, 'nb_books'));
+       
+          $nb= count($this->authors);
+          $sum = array_sum(array_column($this->authors, 'nb_books'));
         return $this->render('author/list.html.twig', [
-            'list' => $authors, 'sum'=>$sum
+            'list' => $this->authors, 'sum'=>$sum
         ]);
     }
+    
     #[Route('/authorDetail/{id}', name: 'author_detail')]
     public function authorDetail($id): Response
     {
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),
-            );
-            foreach ($authors as $auth) {
+        
+            foreach ($this->authors as $auth) {
                 if ($auth['id'] == $id) {
                     $authById = $auth;
                     break;
